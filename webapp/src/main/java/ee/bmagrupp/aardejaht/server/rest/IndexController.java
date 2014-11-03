a181ea205fec4563e81ed8f0f1281d84b6f64c35
TKasekamp <toniskasekamp@gmail.com>
2014-11-04 01:08:25 +0200

    Disable error page
    It got in the way

--- webapp/src/main/java/ee/bmagrupp/aardejaht/server/rest/IndexController.java
+++ webapp/src/main/java/ee/bmagrupp/aardejaht/server/rest/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class IndexController implements ErrorController{
 
     private static final String PATH = "/error";
