64506adc9d9625aab37861d08b2993e9ab692ed5
Sander <sander.tiganik@gmail.com>
2014-11-16 13:44:02 +0200

    small changes

--- webapp/src/test/java/ee/bmagrupp/georivals/server/service/ProfileServiceIntegrationTest.java
+++ webapp/src/test/java/ee/bmagrupp/georivals/server/service/ProfileServiceIntegrationTest.java
@@ -45,7 +45,7 @@ public class ProfileServiceIntegrationTest {
 		assertEquals("Player name", "Mr. TK", player.getUsername());
 		assertEquals("Player id", 1, player.getId());
 		assertEquals("Player email", "mr.tk@pacific.ee", player.getEmail());
-		assertEquals("Player name", 1, player.getOwnedProvinces());
+		assertEquals("Player name", 2, player.getOwnedProvinces());
 		assertEquals("Player units", 19, player.getTotalUnits());
 	}
 
@@ -64,7 +64,7 @@ public class ProfileServiceIntegrationTest {
 		assertEquals("Player name", "Mr. TK", player.getUsername());
 		assertEquals("Player id", 1, player.getId());
 		assertEquals("Player email", "mr.tk@pacific.ee", player.getEmail());
-		assertEquals("Player name", 1, player.getOwnedProvinces());
+		assertEquals("Player name", 2, player.getOwnedProvinces());
 		assertEquals("Player units", 19, player.getTotalUnits());
 	}
 
