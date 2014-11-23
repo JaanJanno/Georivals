fa2feada5b936554b26f7ba53de5ac8f63525e46
Tõnis Kasekamp <toniskasekamp@gmail.com>
2014-11-23 23:28:22 +0200

    BattleHistory nullpointer fix

--- webapp/src/main/java/ee/bmagrupp/georivals/server/service/imlp/BattleServicImpl.java
+++ webapp/src/main/java/ee/bmagrupp/georivals/server/service/imlp/BattleServicImpl.java
@@ -15,45 +15,66 @@ import ee.bmagrupp.georivals.server.core.repository.PlayerRepository;
 import ee.bmagrupp.georivals.server.rest.domain.BattleHistoryDTO;
 import ee.bmagrupp.georivals.server.rest.domain.BattleType;
 import ee.bmagrupp.georivals.server.service.BattleService;
+import ee.bmagrupp.georivals.server.util.Constants;
+import ee.bmagrupp.georivals.server.util.GeneratorUtil;
 
 @Service
 public class BattleServicImpl implements BattleService {
-	
+
 	@Autowired
 	BattleHistoryRepository battleRepo;
-	
+
 	@Autowired
 	OwnershipRepository ownerRepo;
-	
+
 	@Autowired
 	PlayerRepository playerRepo;
-	
+
 	@Override
 	public List<BattleHistoryDTO> getBattles(String cookie) {
-		List <BattleHistory> lst = battleRepo.findBySid(cookie);
+		List<BattleHistory> lst = battleRepo.findBySid(cookie);
 		Player requestMaker = playerRepo.findBySid(cookie);
 		ArrayList<BattleHistoryDTO> rtrn = new ArrayList<BattleHistoryDTO>();
-		for(BattleHistory h : lst){
+		for (BattleHistory h : lst) {
 			boolean attackerWon = h.isAttackerWon();
 			Ownership a = ownerRepo.findByProvinceId(h.getLocation().getId());
-			if(requestMaker.getId() == h.getAttacker().getId()){
-				if(attackerWon){
-					rtrn.add(new BattleHistoryDTO(h.getId(), a.getProvinceName(), h.getDefender().getUserName()
-							, h.getAttackerStrength(), h.getDefenderStrength(), h.getAttackerLosses(), h.getDefenderLosses(),BattleType.ATTACK_PLAYER_WON));
-				}
-				else{
-					rtrn.add(new BattleHistoryDTO(h.getId(), a.getProvinceName(), h.getDefender().getUserName()
-							, h.getAttackerStrength(), h.getDefenderStrength(), h.getAttackerLosses(), h.getDefenderLosses(),BattleType.ATTACK_PLAYER_LOST));
-				}
+			// If the province was not conquered from the BOT, then the
+			// ownership will be null
+			String provinceName = "";
+			if (a == null) {
+				provinceName = GeneratorUtil.generateString(
+						Constants.PROVINCE_NAME_LENGTH, h.getLocation()
+								.getLatitude(), h.getLocation().getLongitude());
+			} else {
+				provinceName = a.getProvinceName();
 			}
-			else{
-				if(attackerWon){
-					rtrn.add(new BattleHistoryDTO(h.getId(), a.getProvinceName(), h.getAttacker().getUserName()
-							, h.getDefenderStrength(), h.getAttackerStrength(), h.getDefenderLosses(), h.getAttackerLosses(), BattleType.DEFENCE_PLAYER_LOST));
+			if (requestMaker.getId() == h.getAttacker().getId()) {
+				if (attackerWon) {
+					rtrn.add(new BattleHistoryDTO(h.getId(), provinceName, h
+							.getDefender().getUserName(), h
+							.getAttackerStrength(), h.getDefenderStrength(), h
+							.getAttackerLosses(), h.getDefenderLosses(),
+							BattleType.ATTACK_PLAYER_WON));
+				} else {
+					rtrn.add(new BattleHistoryDTO(h.getId(), provinceName, h
+							.getDefender().getUserName(), h
+							.getAttackerStrength(), h.getDefenderStrength(), h
+							.getAttackerLosses(), h.getDefenderLosses(),
+							BattleType.ATTACK_PLAYER_LOST));
 				}
-				else{
-					rtrn.add(new BattleHistoryDTO(h.getId(), a.getProvinceName(), h.getAttacker().getUserName()
-							, h.getDefenderStrength(), h.getAttackerStrength(), h.getDefenderLosses(), h.getAttackerLosses(), BattleType.DEFENCE_PLAYER_WON));
+			} else {
+				if (attackerWon) {
+					rtrn.add(new BattleHistoryDTO(h.getId(), provinceName, h
+							.getAttacker().getUserName(), h
+							.getDefenderStrength(), h.getAttackerStrength(), h
+							.getDefenderLosses(), h.getAttackerLosses(),
+							BattleType.DEFENCE_PLAYER_LOST));
+				} else {
+					rtrn.add(new BattleHistoryDTO(h.getId(), provinceName, h
+							.getAttacker().getUserName(), h
+							.getDefenderStrength(), h.getAttackerStrength(), h
+							.getDefenderLosses(), h.getAttackerLosses(),
+							BattleType.DEFENCE_PLAYER_WON));
 				}
 			}
 		}
